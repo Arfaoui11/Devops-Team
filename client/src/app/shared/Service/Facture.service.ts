0b7700731871b1c5fb69d949a8651edd79bffaae
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-29 03:19:42 +0100

    new

--- client/src/app/shared/Service/Facture.service.ts
+++ client/src/app/shared/Service/Facture.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FactureService {
-  readonly API_URL = 'http://192.168.1.11:30687/SpringMVC/facture';
+  readonly API_URL = 'http://192.168.1.11:31000/SpringMVC/facture';
 
   constructor(private httpClient: HttpClient) { }
 
--- client/src/app/shared/Service/Operateur.service.ts
+++ client/src/app/shared/Service/Operateur.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OperateurService {
-  readonly API_URL = 'http://192.168.1.11:30687/SpringMVC/operateur';
+  readonly API_URL = 'http://192.168.1.11:31000/SpringMVC/operateur';
 
   constructor(private httpClient: HttpClient) { }
 
