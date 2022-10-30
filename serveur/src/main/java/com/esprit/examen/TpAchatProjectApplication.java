a3ce84be39f74416b201c8f015f54db781d10671
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-10-30 18:36:46 +0100

    new

--- serveur/src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ serveur/src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,11 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @RestController
 public class TpAchatProjectApplication {
 
-	@Bean
-	public ModelMapper modelMapper(){
 
-		return new ModelMapper();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpAchatProjectApplication.class, args);
