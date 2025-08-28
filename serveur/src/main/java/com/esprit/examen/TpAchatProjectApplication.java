61b2c9f3ba55779dca797d0c187d6f313375e06f
dia <M27773051@a>
2025-08-28 11:10:29 +0100

    feat: fix port front

--- serveur/src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ serveur/src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -32,6 +32,6 @@ public class TpAchatProjectApplication {
 
 	@GetMapping("/")
 	public String home(){
-		return "Welcome Home new mahdi change vendredi";
+		return "Welcome From Mahdi Arfaoui projet Test deployement";
 	}
 }
