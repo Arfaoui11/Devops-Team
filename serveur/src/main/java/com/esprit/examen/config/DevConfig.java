4c0a8867e6ddca5b32ac0f55cc3aa675251215dd
dia <M27773051@a>
2025-08-28 11:32:33 +0100

    feat: fix port front

--- serveur/src/main/java/com/esprit/examen/config/DevConfig.java
+++ serveur/src/main/java/com/esprit/examen/config/DevConfig.java
@@ -6,24 +6,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
-
 @Configuration
 @EnableWebMvc
 public class DevConfig implements WebMvcConfigurer {
+
     @Value("${remote.server.url}")
     private String url;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Angular dev
-                        .allowedOrigins(url) // Angular dev
-                        .allowedOrigins("http://app-client:4200") // Angular dev
-                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // All CRUD + OPTIONS
-                        .allowedHeaders("*")        // Allow all headers
-                        .allowCredentials(true);    // Allow credentials (cookies, auth headers)
+                .allowedOrigins(
+                        "http://localhost:4200",    // Angular dev
+                        url,                        // From properties
+                        "http://app-client:4200",   // Docker container
+                        "http://app-client:80"      // Also add port 80
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
