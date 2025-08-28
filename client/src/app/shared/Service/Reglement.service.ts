6e1409ebdb89e628a5b8fd144934ade9d5d48bd2
dia <M27773051@a>
2025-08-28 21:30:22 +0100

    feat: fix edit button function

--- client/src/app/shared/Service/Reglement.service.ts
+++ client/src/app/shared/Service/Reglement.service.ts
@@ -7,7 +7,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ReglementService {
   //readonly API_URL = 'http://10.0.0.10:8089/SpringMVC/reglement';
-  readonly API_URL = 'http://35.180.113.14/api/SpringMVC/reglement';
+  readonly API_URL = '/SpringMVC/reglement';
 
   constructor(private httpClient: HttpClient) {
   }
